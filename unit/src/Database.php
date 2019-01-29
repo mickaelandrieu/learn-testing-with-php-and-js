a8e79997330e236e231065e967fc4bc8323a9b11
Mickaël Andrieu <mickael.andrieu@prestashop.com>
2019-01-29 07:51:47 +0100

    Complete Database class

--- unit/src/Database.php
+++ unit/src/Database.php
@@ -2,7 +2,32 @@
 
 namespace Solvolabs;
 
+/**
+ * This class is used to emulate a fake and naive database.
+ * For training purposes only, don't reuse it.
+ *
+ * @author Mickaël Andrieu <mickael.andrieu@solvolabs.com>
+ */
 class Database
 {
+    private $users = [
+        [
+            'name' => 'Harry', 'age' => '17', 'title' => 'wizard',
+        ],
+        [
+            'name' => 'Hermione', 'age' => '17', 'title' => 'wizard',
+        ],
+        [
+            'name' => 'Ron', 'age' => '17', 'title' => 'wizard',
+        ],
+    ];
 
+    public function execute($query)
+    {
+        if (!empty($query)) {
+            return $this->users;
+        }
+
+        return [];
+    }
 }
