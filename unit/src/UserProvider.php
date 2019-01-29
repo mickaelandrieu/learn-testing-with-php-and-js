cb306b133ffb456f8ba76bca902bdd13f9a31271
Mickaël Andrieu <mickael.andrieu@prestashop.com>
2019-01-29 07:47:05 +0100

    Fixed UserProvider class

--- unit/src/UserProvider.php
+++ unit/src/UserProvider.php
@@ -4,5 +4,17 @@ namespace Solvolabs;
 
 class UserProvider
 {
+    private $database;
 
+    public function __construct(Database $database)
+    {
+        $this->database = $database;
+    }
+
+    public function getUsers()
+    {
+        $users = $this->database->execute('some-query');
+
+        return json_encode($users);
+    }
 }
